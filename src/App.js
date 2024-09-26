0005cdcdeaac0b6f1b6868b2d252bce461664e1f
zarref-semog <murilogf012@gmail.com>
2024-09-25 21:14:47 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Chart from "./Chart";
+import { getCandles } from "./DataService";
+import useWebSocket from "react-use-websocket";
+import Candle from "./Candle";
 
 function App() {
+  const [symbol, setSymbol] = useState('BTCUSDT');
+  const [interval, setInterval] = useState('1m');
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getCandles(symbol, interval)
+      .then(data => setData(data))
+      .catch(err => err.response ? err.response.data : err.message)
+  }, [symbol, interval]);
+
+  const { lastJsonMessage } = useWebSocket(`wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@kline_${interval}`, {
+    onOpen: () => console.log('Connected to Binance'),
+    onError: (err) => console.log(err),
+    shouldReconnect: () => true,
+    onMessage: () => {
+      if (lastJsonMessage) {
+        const newCandle = new Candle(lastJsonMessage.k.t, lastJsonMessage.k.o, lastJsonMessage.k.h, lastJsonMessage.k.l, lastJsonMessage.k.c);
+        const newData = [...data];
+        if (lastJsonMessage.k.x === false) {
+          newData[newData.length - 1] = newCandle;
+        } else {
+          newData.splice(0, 1);
+          newData.push(newCandle);
+        }
+        setData(newData);
+      }
+    }
+  })
+
+  function onSymbolChange(event) {
+    setSymbol(event.target.value);
+  }
+
+  function onIntervalChange(event) {
+    setInterval(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <select onChange={onSymbolChange}>
+        <option>BTCUSDT</option>
+        <option>ETHUSDT</option>
+        <option>ADAUSDT</option>
+      </select>
+      <select onChange={onIntervalChange}>
+        <option>1m</option>
+        <option>1d</option>
+        <option>1w</option>
+      </select>
+      <Chart data={data}/>
     </div>
   );
 }
---
+++ src/Chart.js
@@ -0,0 +1,32 @@
+import ApexChart from 'react-apexcharts';
+
+/**
+ * props:
+ * - data
+ */
+export default function Chart(props) {
+    const options = {
+        xaxis: {
+            type: 'datetime'
+        },
+        yaxis: {
+            tooltip: {
+                enable: true
+            }
+        }
+    }
+
+    const series = [{
+        data: props.data
+    }];
+
+    return (
+        <ApexChart
+            options={options}
+            series={series}
+            type="candlestick"
+            width={640}
+            height={480}
+        />
+    );
+}
